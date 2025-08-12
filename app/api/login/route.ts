07fab3626d2f252add7ad1224471a3d31e30a437
Ivan monte <ivanmonte07.93@gmail.com>
2025-08-12 15:05:25 -0300

    Seteando el token del login para recibir datos

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -74,11 +74,13 @@ export async function POST(request: Request) {
     });
 
     response.cookies.set('token', token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      path: '/',
-      maxAge: 60 * 60 * 8 // 8 horas
-    });
+  httpOnly: true,
+  secure: true, // 🔒 obligatorio en Vercel
+  sameSite: 'none', // 🔥 necesario para que el navegador la acepte
+  path: '/',
+  maxAge: 60 * 60 * 8
+});
+
 
     return response;
 
