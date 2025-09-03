e920119fc1bc3da79404e27ca438b8fbe1da5521
Ivan monte <ivanmonte07.93@gmail.com>
2025-09-03 18:09:23 -0300

    Favicon en layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,17 +7,15 @@ import { AuthProvider, useAuth } from '@/context/AuthContext';
 import { usePathname } from 'next/navigation';
 
 function LayoutContent({ children }: { children: React.ReactNode }) {
-  const { isAuthenticated, loading } = useAuth(); // ahora usamos loading
+  const { isAuthenticated, loading } = useAuth();
   const pathname = usePathname();
   const isHome = pathname === '/';
 
-  // ✅ Evitamos renderizar nada hasta confirmar sesión
   if (loading) return null;
 
   return (
     <div className="min-h-screen flex flex-col">
       {isAuthenticated && <Navbar />}
-
       <main className={isHome ? 'flex-grow p-0' : 'flex-grow p-4 md:p-6'}>
         {children}
       </main>
@@ -28,6 +26,12 @@ function LayoutContent({ children }: { children: React.ReactNode }) {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="es">
+      <head>
+  <link rel="icon" href="/favicon.ico" />
+  <title>Stocking</title>
+</head>
+
+
       <body>
         <QueryProvider>
           <AuthProvider>
